8065f7d63e573e395619cec981a24c01e15ecc06
ya790206 <ya790206@gmail.com>
2013-05-15 21:34:28 +0800

    Add custom lib

---
+++ Include/kao.h
@@ -0,0 +1,14 @@
+/* Interface to execute compiled code */
+
+#ifndef Kao_H
+#define Kao_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+PyAPI_FUNC(int) Kao_Test();
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* !Py_EVAL_H */
---
+++ Objects/kao.c
@@ -0,0 +1,22 @@
+#include "Python.h"
+int Kao_Test(){
+    PyObject *item;
+    PyObject *obj = PySys_GetObject("argv");
+    PyObject *hack = PyString_FromString("hack");
+    Py_ssize_t size;
+    int flag = 0;
+    int result = 0;
+    if ( obj != NULL){
+        size = PyList_Size(obj);
+        Py_ssize_t i;
+        for(i = 0; i < size ; i++){
+            item = PyList_GetItem(obj, i);
+            if ( PyObject_Cmp(item, hack, &result) != -1){
+                if (result == 0){
+                    flag = 1;
+                }
+            }
+        }
+    }
+    return flag;
+}
